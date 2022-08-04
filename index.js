488c2b09d5013609092909404ce3417952d9b87a
Abdallah Sow <abdallahsow700@gmail.com>
2022-08-04 17:05:13 +0300

    onclick attributed added

--- index.js
+++ index.js
@@ -24,14 +24,23 @@ console.log(myDogAge);
 // Create a variable, bonusPoints. Initialize it as 50. Increase it to 100.
 // Decrease it down to 25, and then finally increase it to 70
 
-let bonusPoints = 50;
-console.log(bonusPoints);
+ let bonusPoints = 50;
+ console.log(bonusPoints);
 
-bonusPoints = bonusPoints + 50;
-console.log(bonusPoints);
+ bonusPoints = bonusPoints + 50;
+ console.log(bonusPoints);
 
-bonusPoints = bonusPoints - 75;
-console.log(bonusPoints);
+ bonusPoints = bonusPoints - 75;
+ console.log(bonusPoints);
 
-bonusPoints = bonusPoints + 45;
-console.log(bonusPoints);
+ bonusPoints = bonusPoints + 45;
+ console.log(bonusPoints);
+
+// initialize the count as 0
+// listen for clicks on the increment button
+// increment the count variable when the button is clicked
+// change the count-el in the HTML to reflect the new count
+
+function increment() {
+    console.log("The button was triggered");
+}
