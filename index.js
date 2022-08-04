dd9e830a9696dd683ffeaec13832f02a636bad68
Abdallah Sow <abdallahsow700@gmail.com>
2022-08-04 14:52:33 +0300

    first js varibale created

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-document.getElementById("count-el").innerText = 5;
+// document.getElementById("count-el").innerText = 5;
+
+let count = 10;
+
+console.log(count);
+
+let myAge = 20;
+
+console.log(myAge);
