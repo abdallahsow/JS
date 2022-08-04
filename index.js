ac9e610ebac2a399cf1a71f8eaaee8691a7f55c6
Abdallah Sow <abdallahsow700@gmail.com>
2022-08-04 15:10:21 +0300

    mathematical operation challenge

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 // document.getElementById("count-el").innerText = 5;
 
-let count = 10;
+// let firstBatch = 8;
+// let secondBatch = 13;
+// let count = firstBatch + secondBatch;
+// console.log(count);
+// let myAge = 20
+// console.log(myAge);
 
-console.log(count);
+// Challenge 1:
+// 1. Create two variables, myAge and humanDogRatio
+// 2. Multiply the two together and store the result in myDogAge
+// 3. Log myDogAge to the console
 
 let myAge = 20;
+let humanDogRatio = 7;
 
-console.log(myAge);
+let myDogAge = myAge * humanDogRatio
+
+console.log(myDogAge);
