40226066fab7b310e3c9925adb002958fa610dad
Abdallah Sow <abdallahsow700@gmail.com>
2022-08-04 15:38:25 +0300

    second challenged dealt with

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 // let secondBatch = 13;
 // let count = firstBatch + secondBatch;
 // console.log(count);
+
 // let myAge = 20
 // console.log(myAge);
 
@@ -15,6 +16,22 @@
 let myAge = 20;
 let humanDogRatio = 7;
 
-let myDogAge = myAge * humanDogRatio
+let myDogAge = myAge * humanDogRatio;
 
 console.log(myDogAge);
+
+// Challenge 2:
+// Create a variable, bonusPoints. Initialize it as 50. Increase it to 100.
+// Decrease it down to 25, and then finally increase it to 70
+
+let bonusPoints = 50;
+console.log(bonusPoints);
+
+bonusPoints = bonusPoints + 50;
+console.log(bonusPoints);
+
+bonusPoints = bonusPoints - 75;
+console.log(bonusPoints);
+
+bonusPoints = bonusPoints + 45;
+console.log(bonusPoints);
