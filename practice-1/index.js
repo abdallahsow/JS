289bf6d36de1b6709ca99d07baa84a150a4bed88
Abdallah Sow <abdallahsow700@gmail.com>
2022-08-06 13:50:24 +0300

    incrementing and decrementing operational

--- practice-1/index.js
+++ practice-1/index.js
@@ -1,5 +1,37 @@
-let firstName = "Abdallah ";
-let secondName = "Sow";
-let fullName = firstName + secondName
+// Challenge 1
+// let firstName = "Abdallah ";
+// let secondName = "Sow";
+// let fullName = firstName + secondName
 
-console.log(fullName);
+// console.log(fullName);
+
+// Challenge 2
+// let name = "Linda";
+// let greeting = "Hi there"
+
+// function greet() {
+//     let friendly = greeting + "," + " "+ name + "!";
+//     console.log(friendly);
+// }
+
+// greet();
+
+
+// Challenge 3
+let myPoints = 3;
+
+function add3Points() {
+    myPoints += 3;
+}
+
+function remove1Point() {
+    myPoints -= 1;
+}
+
+add3Points();
+add3Points();
+add3Points();
+remove1Point();
+remove1Point();
+
+console.log(myPoints);
