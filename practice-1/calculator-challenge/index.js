4b14ec01f72d121ef39b924e1aad3f2db8f2dd9e
Abdallah Sow <abdallahsow700@gmail.com>
2022-08-06 17:12:13 +0300

    made a simple calculator

---
+++ practice-1/calculator-challenge/index.js
@@ -0,0 +1,27 @@
+let num1 = 8;
+let num2 = 2;
+let finalResult = 0
+
+document.getElementById("num1-el").textContent = num1;
+document.getElementById("num2-el").textContent = num2;
+let resultEl = document.getElementById("result-el")
+
+function add() {
+    finalResult = num1 + num2;
+    resultEl.textContent ="Sum: " +  finalResult
+}
+
+function substract() {
+    finalResult = num1 - num2;
+    resultEl.textContent = "Sum: " + finalResult;
+}
+
+function divide() {
+    finalResult = num1 / num2;
+    resultEl.textContent = "Sum: " + finalResult;
+}
+
+function multiply() {
+    finalResult = num1 * num2;
+    resultEl.textContent = "Sum: " + finalResult;
+}
