49fb906c3c0041ed3acbc48d0cd360ea1ae7b11f
Abdallah Sow <abdallahsow700@gmail.com>
2022-08-08 14:59:34 +0300

    challenge 2 completed about if...else conditional

--- blackjack/index.js
+++ blackjack/index.js
@@ -1,3 +1,4 @@
+// Challenge 1
 let firstCard = 2
 let secondCard = 5
 let sum = firstCard + secondCard
@@ -10,4 +11,21 @@ if (sum < 21) {
     console.log("Wohoo! You've got a Blackjack! 🥳");
 } else {
     console.log("You're out of the game! 😭");
+}
+
+// Challenge 2
+// Check if the person is elegible for a birthday card from the King! (100)
+
+let age = 100
+
+// if less than 100    -> "Not elegible"
+// else if exactly 100 -> "Here is your birthday card from the King!"
+// else                -> "Not elegible, you have already gotten one"
+
+if (age < 100) {
+    console.log("Not elegible")
+} else if (age === 100) {
+    console.log("Here is your birthday card from the King!")
+} else {
+    console.log("Not elegible, you have already gotten one")
 }
