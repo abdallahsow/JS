221d927b42410bce10251dae431f765550e65794
Abdallah Sow <abdallahsow700@gmail.com>
2022-08-11 23:59:49 +0300

    myLeads array updated

--- chrome-extension/index.js
+++ chrome-extension/index.js
@@ -1,3 +1,7 @@
-function clickBtn() {
-    console.log("Button clicked!");
-}
+myLeads = []
+const inputEl = document.querySelector("#input-el");
+const inputBtn = document.querySelector("#input-btn");
+inputBtn.addEventListener("click", function () {
+    myLeads.push(inputEl.value)
+    console.log(myLeads)
+})
