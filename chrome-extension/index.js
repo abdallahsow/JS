2716ae1760eb7336e2fd914dd1c8120b937856b1
Abdallah Sow <abdallahsow700@gmail.com>
2022-08-12 09:47:36 +0300

    added an unordered list element

--- chrome-extension/index.js
+++ chrome-extension/index.js
@@ -1,6 +1,8 @@
 myLeads = []
 const inputEl = document.querySelector("#input-el");
 const inputBtn = document.querySelector("#input-btn");
+const ulEl = document.getElementById("ul-el");
+
 inputBtn.addEventListener("click", function () {
     myLeads.push(inputEl.value)
     console.log(myLeads)
