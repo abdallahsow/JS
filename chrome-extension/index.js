e4b5aa9798f423eaf09b3ca7a4bb3f0bfd47e8a6
Abdallah Sow <abdallahsow700@gmail.com>
2022-08-12 11:12:29 +0300

    added a list tag through JS

--- chrome-extension/index.js
+++ chrome-extension/index.js
@@ -1,4 +1,4 @@
-myLeads = []
+myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"];
 const inputEl = document.querySelector("#input-el");
 const inputBtn = document.querySelector("#input-btn");
 const ulEl = document.getElementById("ul-el");
@@ -7,3 +7,7 @@ inputBtn.addEventListener("click", function () {
     myLeads.push(inputEl.value)
     console.log(myLeads)
 })
+
+for (let i = 0; i < myLeads.length; i++) {
+    ulEl.innerHTML += "<li>" + myLeads[i] + "</li>";
+}
