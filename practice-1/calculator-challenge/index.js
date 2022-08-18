4e711364b53609376df8d8957f12c01e4690f03d
Abdallah Sow <abdallahsow700@gmail.com>
2022-08-18 12:24:38 +0300

    fixed the operations

--- practice-1/calculator-challenge/index.js
+++ practice-1/calculator-challenge/index.js
@@ -1,30 +1,26 @@
-let num1 = "";
-let num2 = "";
 let finalResult = 0
-const firstTerm = num1.value
-const secondTerm = num2.value;
+num1 = document.getElementById("num1-el");
+num2 = document.getElementById("num2-el");
 
- num1 = document.getElementById("num1-el");
- num2 = document.getElementById("num2-el");
-let resultEl = document.getElementById("result-el")
+const resultEl = document.getElementById("result-el")
+
 
 function add() {
-    first
-    finalResult = firstTerm + secondTerm;
-    resultEl.textContent ="Sum: " +  finalResult
+    finalResult = Number(num1.value) + Number(num2.value);
+    resultEl.textContent = "Sum: " + finalResult;
 }
 
 function substract() {
-    finalResult = firstTerm - secondTerm;
+    finalResult = Number(num1.value) - Number(num2.value);
     resultEl.textContent = "Diff: " + finalResult;
 }
 
 function divide() {
-    finalResult = firstTerm / secondTerm;
+    finalResult = Number(num1.value) / Number(num2.value);
     resultEl.textContent = "Qt: " + finalResult;
 }
 
 function multiply() {
-    finalResult = firstTerm * secondTerm;
+    finalResult = Number(num1.value) * Number(num2.value);
     resultEl.textContent = "Pr: " + finalResult;
 }
