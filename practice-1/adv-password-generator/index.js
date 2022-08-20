6009546220022fbc4b30ced120371cc43faa39e2
Abdallah Sow <abdallahsow700@gmail.com>
2022-08-20 21:48:13 +0300

    added the clipboard button

--- practice-1/adv-password-generator/index.js
+++ practice-1/adv-password-generator/index.js
@@ -1,13 +1,36 @@
 const firstPass = document.getElementById("first-pass");
 const secondPass = document.getElementById("second-pass");
+const upperCase = document.getElementById("uppercase-choice");
+const lowerCase = document.getElementById("lowercase-choice");
+const numbers = document.getElementById("numbers-choice");
+const symbols = document.getElementById("symbols-choice");
+let random = ""
 
-function generatePass(passLength) {
-  firstPass.textContent = "";
-  secondPass.textContent = "";
-  for (let i = 0; i < passLength; i++) {
-    let randomOnePass = String.fromCharCode(Math.floor(Math.random() * 94) + 33);
-    firstPass.textContent += randomOnePass;
-    let randomTwoPass = String.fromCharCode(Math.floor(Math.random() * 94) + 33);
-    secondPass.textContent += randomTwoPass;
-  }
+// function generatePass(passLength) {
+//   firstPass.textContent = "";
+//   secondPass.textContent = "";
+//   for (let i = 0; i < passLength; i++) {
+//     let randomOnePass = String.fromCharCode(Math.floor(Math.random() * 94) + 33);
+//     firstPass.textContent += randomOnePass;
+//     let randomTwoPass = String.fromCharCode(Math.floor(Math.random() * 94) + 33);
+//     secondPass.textContent += randomTwoPass;
+//   }
+// }
+
+function cases(availability, charCode) {
+  random = String.fromCharCode(
+    Math.floor(Math.random() * availability) + charCode
+  );
+  console.log(random)
 }
+
+function symbols(availability, charCode) {
+  random = String.fromCharCode(
+    Math.floor(Math.random() * availability) + charCode
+  );
+  console.log(random)
+}
+
+
+
+
