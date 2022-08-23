cec26b578655d6b01b8a7869d790992d948f6c92
Abdallah Sow <abdallahsow700@gmail.com>
2022-08-23 15:58:14 +0300

    modified the template-literals folder

--- ES6+/template-literals/index.js
+++ ES6+/template-literals/index.js
@@ -1,8 +1,8 @@
 let word1 = "Hello";
 let word2 = "World";
-const greeting = `${word1} 
+const greeting = `${word1}
 ${word2}`;
 console.log(greeting)
 
-const final = "Hello \n World";
+const final = "Hello \n" + "World";
 console.log(final);
