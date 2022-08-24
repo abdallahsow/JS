dc41ff8cb543f7d2283655cfb7fc6025e2acfaab
Abdallah Sow <abdallahsow700@gmail.com>
2022-08-24 10:38:25 +0300

    tried the import/export challenge

---
+++ ES6+/import-export/index.js
@@ -0,0 +1,10 @@
+import { add, substract, multiply, divide } from "./data.js";
+
+function logging() {
+  console.log(add(5, 10));
+  console.log(substract(10, 5));
+  console.log(multiply(5, 10));
+  console.log(divide(10, 2));
+}
+
+logging();
