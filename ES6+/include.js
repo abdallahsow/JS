9a2b06616d2c20d196046cd4e33c82e25fcf6957
Abdallah Sow <abdallahsow700@gmail.com>
2022-08-24 12:09:24 +0300

    indentation fixed

--- ES6+/include.js
+++ ES6+/include.js
@@ -1,7 +1,9 @@
-const listIngredients = ["eggs", "flour", "sugar", "butter",]
+const listIngredients = ["eggs", "flour", "sugar", "butter"];
 
 if (listIngredients.includes("chocolate")) {
-    console.log("We are going to make a chocolate cake!");
+  console.log("We are going to make a chocolate cake!");
 } else {
-    console.log("We can't make a chocolate cake because we are missing the ingredient chocolate!");
+  console.log(
+    "We can't make a chocolate cake because we are missing the ingredient chocolate!"
+  );
 }
