34057574b8592bf76542187f8a870c864eecafcf
Abdallah Sow <abdallahsow700@gmail.com>
2022-08-26 10:43:44 +0300

    added some JSX  to the React Site

---
+++ React/React Site/index.js
@@ -0,0 +1,12 @@
+const navigation = (
+  <nav>
+    <h1>React Benefits</h1>
+    <ul>
+      <li>Pricing</li>
+      <li>About</li>
+      <li>Contact</li>
+    </ul>
+  </nav>
+);
+
+ReactDOM.render(navigation, document.querySelector("#root"))
