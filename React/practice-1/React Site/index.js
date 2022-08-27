c67ccd7ab78e679fa502ef401cc8ea80541ff16a
Abdallah Sow <abdallahsow700@gmail.com>
2022-08-27 20:08:10 +0300

    used custom component for React Site

--- React/practice-1/React Site/index.js
+++ React/practice-1/React Site/index.js
@@ -1,12 +1,16 @@
-const navigation = (
-  <nav>
-    <h1>React Benefits</h1>
-    <ul>
-      <li>Pricing</li>
-      <li>About</li>
-      <li>Contact</li>
-    </ul>
-  </nav>
-);
-const root = ReactDOM.createRoot(document.querySelector("#root"));
-root.render(navigation);
+function site() {
+  return (
+    <nav>
+      <h1>React Benefits</h1>
+      <ul>
+        <li>Pricing</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+  
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(site());
