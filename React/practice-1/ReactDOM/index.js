2ea0d8ee3b9d85afd4886f8587f56a5d91d05360
Abdallah Sow <abdallahsow700@gmail.com>
2022-08-27 20:10:14 +0300

    used custom component for better flexibility

--- React/practice-1/ReactDOM/index.js
+++ React/practice-1/ReactDOM/index.js
@@ -1,16 +1,18 @@
-const page = (
-  <div>
-    <h1>Example of frameworks</h1>
-    <ul>
-      <li>React.JS</li>
-      <li>Angular.JS</li>
-      <li>View.JS</li>
-      <li>Next.JS</li>
-      <li>Node.JS</li>
-      <li>Express.JS</li>
-    </ul>
-  </div>
-);
+const page = function () {
+  return (
+    <div>
+      <h1>Example of frameworks</h1>
+      <ul>
+        <li>React.JS</li>
+        <li>Angular.JS</li>
+        <li>View.JS</li>
+        <li>Next.JS</li>
+        <li>Node.JS</li>
+        <li>Express.JS</li>
+      </ul>
+    </div>
+  );
+};
 
 const divRoot = ReactDOM.createRoot(document.getElementById("root"));
-divRoot.render(page);
+divRoot.render(page());
