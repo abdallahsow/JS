80fd992e338a0cfa0ffe0d07370c68717ce060b6
Abdallah Sow <abdallahsow700@gmail.com>
2022-09-07 10:27:58 +0300

    updated practice-3

--- practice-3/array.map().js
+++ practice-3/array.map().js
@@ -1,3 +1,34 @@
+/*
+Challenge 1:
+Given an array of numbers, return an array of each number, squared
+*/
+
 const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-const squaredNums = nums.map(num => num ** 2);
-console.log(squaredNums)
+const squaredNums = nums.map((num) => num ** 2);
+console.log(squaredNums);
+
+/*
+Challenge 2:
+Given an array of strings, return an array where 
+the first letter of each string is capitalized
+*/
+
+const names = ["alice", "bob", "tom", "frank", "charlie", "danielle"];
+const capitalizedNames = names.map(
+  (name) => name[0].toUpperCase() + name.slice(1)
+);
+
+console.log(capitalizedNames);
+
+/*
+Challenge 3:
+Given an array of strings, return an array of strings that wraps each
+of the original strings in an HTML-like <p></p> tag.
+*/
+
+const pokemon = ["Bulbasaur", "Charmander", "Squirtle", "Pikachu"];
+const paragraphs = pokemon.map(function (pok) {
+  return `<p>${pok}</p>`;
+});
+
+console.log(paragraphs);
